937ee5cb91d965a5117fb29039dfa4ddafd74d6b
Theo Dowling <thdowling@gmail.com>
2019-12-08 17:52:59 +0200

    Added solution for day 6

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,59 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+  alias AdventOfCode.Utils.Input
+
+  def part1(inputs) do
+    inputs
+    |> Input.parse_lines()
+    |> Enum.reduce(%{}, fn a, acc ->
+      [l, r] = String.split(a, ")")
+      Map.update(acc, l, [r], &[r | &1])
+    end)
+    |> count_connections("COM", 1, 0)
   end
 
-  def part2(_args) do
+  def count_connections(_map, nil, _, acc), do: acc
+
+  def count_connections(map, k, i, acc) do
+    start = Map.get(map, k)
+
+    if start != nil do
+      Enum.reduce(start, acc, fn key, acc ->
+        acc + i + count_connections(map, key, i + 1, 0)
+      end)
+    else
+      acc
+    end
+  end
+
+  def part2(inputs) do
+    inputs
+    |> Input.parse_lines()
+    |> Enum.reduce(%{}, fn a, acc ->
+      [l, r] = String.split(a, ")")
+      Map.update(acc, l, [r], &[r | &1])
+    end)
+    |> map_routes("COM", "COM")
+    |> List.flatten()
+    |> find_san_and_you()
+  end
+
+  def map_routes(map, k, acc) do
+    values = Map.get(map, k)
+
+    if values != nil,
+      do: Enum.map(values, fn v -> map_routes(map, v, "#{acc}:#{v}") end),
+      else: acc
+  end
+
+  def find_san_and_you(list) do
+    [a, b] =
+      Enum.filter(list, fn x -> String.contains?(x, "YOU") or String.contains?(x, "SAN") end)
+      |> Enum.map(fn x -> String.split(x, ":") end)
+
+    a_map = MapSet.new(a)
+    b_map = MapSet.new(b)
+
+    unique_items = MapSet.union(MapSet.difference(a_map, b_map), MapSet.difference(b_map, a_map))
+    MapSet.size(unique_items) - 2
   end
 end
